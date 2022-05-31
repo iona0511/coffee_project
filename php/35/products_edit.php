6435296286df71daef4c6fd0fed50cbe6ae407e7
GB-Jun <windgb369@hotmail.com>
2022-05-31 16:36:06 +0800

    edit page and api WIP

---
+++ php/35/products_edit.php
@@ -0,0 +1,202 @@
+<?php require dirname(dirname(__DIR__, 1)) . '/parts/connect_db.php';
+$pageName = 'products_edit';
+$title = '編輯商品資料';
+
+$products_sid = isset($_GET['products_sid']) ? intval($_GET['products_sid']) : 0;
+if (empty($products_sid)) {
+    header('Location: products.php');
+
+    exit;
+}
+
+$row = $pdo->query("SELECT * FROM`products`
+                    JOIN `products_categroies` 
+                        ON`products`.`products_with_products_categroies_sid` = `products_categroies`.`products_categroies_sid`
+                    JOIN `products_pic` 
+                        ON`products`.`products_with_products_pic` = `products_pic`.`products_pic_sid`
+                    JOIN `products_style_filter`
+                        ON`products`.`products_with_products_style_filter_sid` = `products_style_filter`.`products_style_filter_sid`
+                    WHERE products_sid=$products_sid")->fetch();                    
+$row_cate = $pdo->query("SELECT * FROM`products_categroies`")->fetchAll();
+$row_pic = $pdo->query("SELECT * FROM`products_pic`")->fetchAll();
+$row_style = $pdo->query("SELECT * FROM`products_style_filter`")->fetchAll();
+
+if (empty($row)) {
+    header('Location: products.php');
+    echo "console.log('a')";
+    exit;
+}
+
+
+
+?>
+<?php include dirname(dirname(__DIR__, 1)) . '/parts/html-head.php' ?>
+<?php include dirname(dirname(__DIR__, 1)) . '/parts/navbar.php' ?>
+<style>
+    .form-control.red {
+        border: 1px solid red;
+    }
+
+    .form-text.red {
+        color: red;
+    }
+</style>
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">編輯資料</h5>
+                    <form name="form1" onsubmit="sendData();return false;" novalidate>
+                        <input type="hidden" name="products_sid" value="<?= $row['products_sid'] ?>">
+
+                        <div class="mb-3">
+                            <label for="products_name" class="form-label">* 商品名稱</label>
+                            <input type="text" class="form-control" id="products_name" name="products_name" required value="<?= htmlentities($row['products_name']) ?>">
+                            <div class="form-text red"></div>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="products_introduction" class="form-label">商品簡介</label>
+                            <input type="products_introduction" class="form-control" id="products_introduction" name="products_introduction" value="<?= $row['products_introduction'] ?>">
+                            <div class="form-text red"></div>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="products_detail_introduction" class="form-label">商品詳細介紹</label>
+                            <textarea class="form-control" name="products_detail_introduction" id="products_detail_introduction" cols="30" rows="3"><?= htmlentities($row['products_detail_introduction']) ?></textarea>
+                            <div class="form-text red"></div>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="products_price" class="form-label">價錢</label>
+                            <input type="number" class="form-control" id="products_price" name="products_price" value="<?= $row['products_price'] ?>">
+                            <div class="form-text"></div>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="products_forsale" class="form-label">是否販賣中</label>
+                            <input type="number" class="form-control" id="products_forsale" name="products_forsale" value="<?= $row['products_forsale'] ?>">
+                            <div class="form-text"></div>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="products_onsale" class="form-label">是否有特價</label>
+                            <input type="number" class="form-control" id="products_onsale" name="products_onsale" value="<?= $row['products_onsale'] ?>">
+                            <div class="form-text"></div>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="products_stocks" class="form-label">商品庫存</label>
+                            <input type="number" class="form-control" id="products_stocks" name="products_stocks" value="<?= $row['products_stocks'] ?>">
+                            <div class="form-text"></div>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="products_categroies_sid" class="form-label">商品分類</label>
+                            <select name="products_categroies_sid" id="products_categroies_sid">
+                                <option value="">-- 請選擇 --</option>
+                                <?php foreach ($row_cate as $r) : ?>
+                                    <option value="<?= $r['products_categroies_sid'] ?>">
+                                        <?= $r['products_categroies_name'] ?>
+                                    </option>
+                                <?php endforeach; ?>
+                            </select>
+                        </div>
+                        
+                        <div class="mb-3">
+                            <label for="products_pic_one" class="form-label">商品圖片(商品頁)</label>
+                            <div class="form-text"></div>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="products_pic_multi" class="form-label">商品圖片(詳細頁)</label>
+                            <div class="form-text"></div>
+                        </div>
+
+                        <div class="mb-3">
+                            <label for="products_style_filter_sid" class="form-label">商品風格</label>
+                            <select name="products_style_filter_sid" id="products_style_filter_sid">
+                                <option value="">-- 請選擇 -- </option>
+                                <?php foreach ($row_style as $r) : ?>
+                                    <option value="<?= $r['products_style_filter_sid'] ?>">
+                                        <?= $r['products_style_filter_categroies'] ?>
+                                    </option>
+                                <?php endforeach; ?>
+                            </select>
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">修改</button>
+                    </form>
+                    <div id="info-bar" class="alert alert-success" role="alert" style="display:none;">
+                        資料編輯成功
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+<?php dirname(dirname(__DIR__, 1)) . '/parts/scripts.php' ?>
+<script>
+    const row = <?= json_encode($row, JSON_UNESCAPED_UNICODE); ?>;
+
+
+    const info_bar = document.querySelector('#info-bar');
+    const name_f = document.form1.products_name;
+    const price_f = document.form1.products_price;
+    const forsale_f = document.form1.products_forsale;
+
+    const fields = [name_f, price_f, forsale_f];
+    const fieldTexts = [];
+    for (let f of fields) {
+        fieldTexts.push(f.nextElementSibling);
+    }
+
+
+
+    async function sendData() {
+        // 讓欄位的外觀回復原來的狀態
+        for (let i in fields) {
+            fields[i].classList.remove('red');
+            fieldTexts[i].innerText = '';
+        }
+        info_bar.style.display = 'none'; // 隱藏訊息列
+
+        // TODO: 欄位檢查, 前端的檢查
+        let isPass = true; // 預設是通過檢查的
+
+        if (name_f.value.length < 2) {
+            fields[0].classList.add('red');
+            fieldTexts[0].innerText = '商品名稱至少兩個字';
+            isPass = false;
+        }
+
+        if (!isPass) {
+            return; // 結束函式
+        }
+
+        const fd = new FormData(document.form1);
+        const r = await fetch('products_edit_api.php', {
+            method: 'POST',
+            body: fd,
+        });
+
+        const result = await r.json();
+        console.log(result);
+        info_bar.style.display = 'block';
+        if (result.success) {
+            info_bar.classList.remove('alert-danger');
+            info_bar.classList.add('alert-success');
+            info_bar.innerText = '修改成功';
+
+            setTimeout(() => {}, 2000);
+        } else {
+            info_bar.classList.remove('alert-success');
+            info_bar.classList.add('alert-danger');
+            info_bar.innerText = result.error || '資料沒有修改';
+        }
+
+    }
+</script>
+<?php dirname(dirname(__DIR__, 1)) . '/parts/html-foot.php' ?>
---
+++ php/35/products_edit_api.php
@@ -0,0 +1,79 @@
+<?php
+require dirname(dirname(__DIR__, 1)) . '/parts/connect_db.php';
+header('Content-Type: application/json');
+
+$output = [
+    'success' => false,
+    'postData' => $_POST,
+    'code' => 0,
+    'error' => ''
+];
+
+
+$products_sid = isset($_POST['products_sid']) ? intval($_POST['products_sid']) : 0;
+
+if (empty($products_sid) or empty($_POST['products_name'])) {
+    $output['error'] = '沒有商品名稱';
+    $output['code'] = 400;
+    echo json_encode($output, JSON_UNESCAPED_UNICODE);
+    exit;
+}
+
+$products_name = $_POST['products_name'];
+$products_introduction = $_POST['products_introduction'] ?? '';
+$products_detail_introduction = $_POST['products_detail_introduction'] ?? '';
+$products_price = $_POST['products_price'] ?? '';
+$products_forsale = $_POST['products_forsale'] ?? '';
+$products_onsale = $_POST['products_onsale'] ?? '';
+$products_stocks = $_POST['products_stocks'] ?? '';
+$products_categroies_sid = $_POST['products_categroies_sid'] ?? '';
+$products_pic_one = $_POST['products_pic_one'] ?? '';
+$products_pic_multi = $_POST['products_pic_multi'] ?? '';
+$products_style_filter_sid = $_POST['products_style_filter_sid'] ?? '';
+
+// TODO 其他欄位檢查
+
+$sql = "UPDATE `products` 
+SET 
+`products_name`=?, 
+`products_introduction`=?, 
+`products_detail_introduction`=?, 
+`products_price`=?, 
+`products_forsale`=?, 
+`products_onsale`=?, 
+`products_stocks`=?, 
+`products_with_products_categroies_sid`=?, 
+`products_pic_one`=?, 
+`products_pic_multi`=?, 
+`products_with_products_style_filter_sid`=?, 
+WHERE `products_sid`=$products_sid ";
+
+$stmt = $pdo->prepare($sql);
+
+$stmt->execute([
+    $products_name,
+    $products_introduction,
+    $products_detail_introduction,
+    $products_price,
+    $products_forsale,
+    $products_onsale,
+    $products_stocks,
+    $products_categroies_sid,
+    $products_pic_one,
+    $products_pic_multi,
+    $products_style_filter_sid,
+]);
+
+// echo $stmt->rowCount();
+// echo json_encode($output, JSON_UNESCAPED_UNICODE);
+// exit;
+
+
+if ($stmt->rowCount() == 1) {
+    $output['success'] = true;
+} else {
+    $output['error'] = '資料沒有修改';
+    $output['code'] = 211;
+}
+
+echo json_encode($output, JSON_UNESCAPED_UNICODE);
