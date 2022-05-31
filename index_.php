df1fdd88c745a2447ebb0012fa3cd6f842f87aa1
GB-Jun <windgb369@hotmail.com>
2022-05-31 09:03:33 +0800

    add connect_db and move testing page

---
+++ index_.php
@@ -0,0 +1,5 @@
+<?php require __DIR__ . './parts/connect_db.php' ?>
+<?php include __DIR__ . './parts/html-head.php' ?>
+<?php include __DIR__ . './parts/navbar.php' ?>
+<?php include __DIR__ . './parts/scripts.php' ?>
+<?php include __DIR__ . './parts/html-foot.php' ?>
---
+++ parts/html-head.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title><?= $title ?? '來拎+B'?></title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous" />
+    <link rel="stylesheet" href="fontawesome/css/all.css" />
+</head>
