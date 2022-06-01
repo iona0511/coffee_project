15c43cfde2bb9b334d250774ad3fdfce2fa06f9a
hunter850 <hunter850711@gmail.com>
2022-06-01 16:26:20 +0800

    購物車新增優惠卷寫入session

--- php/40/cart_write_session_api.php
+++ php/40/cart_write_session_api.php
@@ -1,7 +1,9 @@
 <?php
     session_start();
     $rawJSON = $_POST["fakeData"];
+    $rawCoupon = $_POST["couponSelect"];
     $_SESSION["rawJSON"] = $rawJSON;
-    $rawData = json_decode($rawJSON,true);
+    $_SESSION["rawCoupon"] = $rawCoupon;
+    $rawData = json_decode($rawCoupon,true);
     echo json_encode($rawData, JSON_UNESCAPED_UNICODE);
 ?>
