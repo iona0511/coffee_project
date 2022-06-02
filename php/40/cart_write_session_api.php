aa414a2a7facaa23bbdb521cfbaa053e685400a9
hunterluo <hunter850711@gmail.com>
2022-06-02 21:45:57 +0800

    購物車表單寫入資料表 尚未加入驗證

--- php/40/cart_write_session_api.php
+++ php/40/cart_write_session_api.php
@@ -2,8 +2,10 @@
     session_start();
     $rawJSON = $_POST["fakeData"];
     $rawCoupon = $_POST["couponSelect"];
+    $displayTotal = $_POST["displayTotal"];
     $_SESSION["rawJSON"] = $rawJSON;
     $_SESSION["rawCoupon"] = $rawCoupon;
+    $_SESSION["displayTotal"] = $displayTotal;
     // $rawData = json_decode($rawCoupon,true);
     // echo json_encode($rawData, JSON_UNESCAPED_UNICODE);
 ?>
