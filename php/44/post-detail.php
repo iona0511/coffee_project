776ab5ec8d2221499fa0322108b5c9e3d9404505
Tommy <sky003428@gmail.com>
2022-06-02 14:02:08 +0800

    detail新增tag顯示

--- php/44/post-detail.php
+++ php/44/post-detail.php
@@ -2,26 +2,28 @@
 require __DIR__ . '/connect_db.php';
 
 
-$sid = isset($_GET['pid']) ? intval($_GET['pid']) : '';
+$pid = isset($_GET['pid']) ? intval($_GET['pid']) : '';
 
 
-// 判斷有沒有sid，沒有id導回前一頁
-if (empty($sid)) {
+// 判斷有沒有pid，沒有id導回前一頁
+if (empty($pid)) {
     header("Location:post-list.php");
 } else {
     //用id進sql判斷有沒有該文章，
-    $t_sql = "SELECT COUNT(1) FROM post WHERE `delete_state`='0' AND `sid`='$sid'";
-    $havePost=$pdo->query($t_sql)->fetch(PDO::FETCH_NUM)[0];
-    if ($havePost==0) {
+    $t_sql = "SELECT COUNT(1) FROM post WHERE `delete_state`='0' AND `sid`='$pid'";
+    $havePost = $pdo->query($t_sql)->fetch(PDO::FETCH_NUM)[0];
+    if ($havePost == 0) {
         header("Location:post-list.php");
     }
 }
 
 
-$sql = sprintf("SELECT * FROM post WHERE `delete_state`='0' AND `sid`=%s", $sid);
+$sql = sprintf("SELECT * FROM post WHERE `delete_state`='0' AND `sid`=%s", $pid);
+$tag_sql = sprintf("SELECT * FROM post_tag WHERE `post_sid`=%s", $pid);
+
 
 $rows = $pdo->query($sql)->fetch();
-
+$tags = $pdo->query($tag_sql)->fetchAll();
 
 // echo json_encode($rows, JSON_UNESCAPED_UNICODE);
 ?>
@@ -44,6 +46,9 @@ $rows = $pdo->query($sql)->fetch();
 <body>
     <div class="container">
         <?= json_encode($rows, JSON_UNESCAPED_UNICODE); ?>
+        <br>
+        <h3>標籤:</h3>
+        <?= json_encode($tags, JSON_UNESCAPED_UNICODE); ?>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
