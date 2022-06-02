d2a47190175a3b2c3dce4b57604d44ef58e3effb
Tommy <sky003428@gmail.com>
2022-06-02 14:04:03 +0800

    完成處理Table Post,Table Tag,Table Post_tag關聯

---
+++ php/44/post-add-api.php
@@ -0,0 +1,95 @@
+<?php
+require __DIR__ . '/connect_db.php';
+
+$op_msg = [
+    'success' => false,
+    'postData' => $_POST,
+    'error' => '',
+    'postId' => 0,
+    'tag' => '',
+];
+
+if (empty($_POST['title'])) {
+    $op_msg['error'] = '沒有文章標題';
+    $op_msg['code'] = 400;
+    echo json_encode($op_msg, JSON_UNESCAPED_UNICODE);
+    exit;
+} elseif (empty($_POST['topic'])) {
+    $op_msg['error'] = '沒有類別';
+    $op_msg['code'] = 400;
+    echo json_encode($op_msg, JSON_UNESCAPED_UNICODE);
+    exit;
+} elseif (empty($_POST['content'])) {
+    $op_msg['error'] = '沒有內文';
+    $op_msg['code'] = 400;
+    echo json_encode($op_msg, JSON_UNESCAPED_UNICODE);
+    exit;
+}
+
+$title = $_POST['title'];
+$topic = $_POST['topic'];
+$content = $_POST['content'];
+$tag = $_POST['tag'] ?? '';
+
+
+
+
+$sql = "INSERT INTO `post`(
+    `title`, `topic_sid`, `content`,
+    `member_nickname`, `member_sid`, `created_at`
+    ) VALUES (
+        ?, ?, ?,
+        ?, ?, NOW()
+    )";
+
+
+
+$stmt = $pdo->prepare($sql);
+
+$stmt->execute([
+    $title, $topic, $content,
+    'Tommy', '666'
+]);
+
+
+
+if ($stmt->rowCount() == 1) {
+    $op_msg['success'] = true;
+    $postSid = $pdo->lastInsertId();
+    $op_msg['postId'] = $postSid;
+
+    // Table Tag處理 //
+    $search_tag_sql = sprintf("SELECT * FROM `tag` WHERE `name` = '%s'", $tag);
+    $tag_in_sql = $pdo->query($search_tag_sql)->fetch();
+
+    // 找tag表內有沒有名稱一樣的tag
+    if (!empty($tag_in_sql)) {
+        $tagTimes = $tag_in_sql['times'] + 1;
+        //紀錄tagSid
+        $tagSid = $tag_in_sql['sid'];
+
+        // 若有此tag tag使用次數+1
+        $sql = sprintf("UPDATE `tag` SET `times` = '%s' WHERE `name` = '%s'", $tagTimes, $tag);
+        $pdo->query($sql);
+        $op_msg['tag'] = "標籤使用${tagTimes}次";
+    } else {
+        $sql = "INSERT INTO `tag` (`name`) VALUES (?)";
+        $stmt = $pdo->prepare($sql);
+
+        $stmt->execute([$tag]);
+        //紀錄tagSid
+        $tagSid = $pdo->lastInsertId();
+        $op_msg['tag'] = "新增標籤${tag}";
+    }
+
+    //Table Post_tag處理
+    //新增post_tag關聯
+    $sql = "INSERT INTO `post_tag` (`post_sid`, `tag_sid`, `tag_name`) 
+    VALUES (?, ?, ?)";
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$postSid, $tagSid, $tag]);
+}
+
+
+
+echo json_encode($op_msg, JSON_UNESCAPED_UNICODE);
