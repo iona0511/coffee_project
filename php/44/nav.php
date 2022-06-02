495cd1d67f8d9c1f6b355452d574a9dc542759ac
Tommy <sky003428@gmail.com>
2022-06-02 14:01:47 +0800

    nav

---
+++ php/44/nav.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-lg navbar-light mb-3" style="background-color: #e3f2fd;">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Post</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarColor03">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="post-list.php">文章列表</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="post-add.php">新增文章</a>
+                    </li>
+                </ul>
+
+            </div>
+        </div>
+    </nav>
