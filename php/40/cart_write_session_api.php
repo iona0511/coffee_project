c082440205df5946d80c40b5b0d5c3669ebf0a59
hunterluo <hunter850711@gmail.com>
2022-06-03 11:06:54 +0800

    cart_write_data_api 刪除註解

--- php/40/cart_write_session_api.php
+++ php/40/cart_write_session_api.php
@@ -1,11 +1,6 @@
 <?php
     session_start();
-    $rawJSON = $_POST["fakeData"];
-    $rawCoupon = $_POST["couponSelect"];
-    $displayTotal = $_POST["displayTotal"];
-    $_SESSION["rawJSON"] = $rawJSON;
-    $_SESSION["rawCoupon"] = $rawCoupon;
-    $_SESSION["displayTotal"] = $displayTotal;
-    // $rawData = json_decode($rawCoupon,true);
-    // echo json_encode($rawData, JSON_UNESCAPED_UNICODE);
+    $_SESSION["rawJSON"] =  $_POST["fakeData"];
+    $_SESSION["rawCoupon"] = $_POST["couponSelect"];
+    $_SESSION["displayTotal"] = $_POST["displayTotal"];
 ?>
