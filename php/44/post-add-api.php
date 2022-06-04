76b42fcbe8922a02503ef25e75d1deb6cdf3a0af
TommyLin <sky003428@gmail.com>
2022-06-04 15:51:42 +0800

    api發文串接會員session,未登入會報錯

--- php/44/post-add-api.php
+++ php/44/post-add-api.php
@@ -1,13 +1,20 @@
 <?php
 require __DIR__ . '/part/connect_db.php';
 
+
+
 $op_msg = [
     'success' => false,
     'postData' => $_POST,
     'error' => '',
     'postId' => 0,
     'tag' => '',
+    'poster' => ''
 ];
+
+$m_nickname = isset($_SESSION['user']['member_nickname']) ? $_SESSION['user']['member_nickname'] : '';
+$m_sid = isset($_SESSION['user']['member_sid']) ? $_SESSION['user']['member_nickname'] : '';
+
 
 if (empty($_POST['title'])) {
     $op_msg['error'] = '沒有文章標題';
@@ -22,6 +29,11 @@ if (empty($_POST['title'])) {
 } elseif (empty($_POST['content'])) {
     $op_msg['error'] = '沒有內文';
     $op_msg['code'] = 400;
+    echo json_encode($op_msg, JSON_UNESCAPED_UNICODE);
+    exit;
+} elseif (empty($m_sid)) {
+    $op_msg['error'] = '請先登入';
+    $op_msg['code'] = 300;
     echo json_encode($op_msg, JSON_UNESCAPED_UNICODE);
     exit;
 }
@@ -47,11 +59,18 @@ $sql = "INSERT INTO `post`(
 
 
 
+
 $stmt = $pdo->prepare($sql);
 
+// $stmt->execute([
+//     $title, $topic, $content,
+//     'Tommy', '666'
+// ]);
+
+//用session 的member發文
 $stmt->execute([
     $title, $topic, $content,
-    'Tommy', '666'
+    $m_nickname, $m_sid
 ]);
 
 
@@ -59,6 +78,7 @@ if ($stmt->rowCount() == 1) {
     $op_msg['success'] = true;
     $postSid = $pdo->lastInsertId();
     $op_msg['postId'] = $postSid;
+    $op_msg['poster'] = $m_sid;
 
     if (!empty($tag)) {
         // Table Tag處理 //
