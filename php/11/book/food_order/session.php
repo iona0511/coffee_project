e2631ff3885520d91ef23b39dbe3d67f53e04280
Iona <user@userdembp.iiiedu.local>
2022-06-06 15:52:21 +0800

    -

--- php/11/book/food_order/session.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-
-$_SESSION ['a']++;
-
-echo $_SESSION['a'];
---
+++ php/11/food_order_api.php
@@ -0,0 +1,11 @@
+<?php 
+require __DIR__ . '/connect_db.php';
+
+
+header('Content-Type:apllication/json');
+
+$menu_sql ="SELECT `menu_sid`, `menu_categories`, `menu_photo`, `menu_name`, `menu_kcal`, `menu_price_m`, `menu_price_l`, `menu_nutrition` FROM `menu`";
+
+$totalRows = $pdo->query($menu_sql)->fetchAll(); 
+
+echo json_encode($totalRows,JSON_UNESCAPED_UNICODE);
