75ce9a65c6feafd403caee12665ee1975d1d329d
hunterluo <hunter850711@gmail.com>
2022-06-09 21:59:24 +0800

    session viewer修正bug

--- php/40/sessionViewer.php
+++ php/40/sessionViewer.php
@@ -6,11 +6,5 @@
     $output["food"] = json_decode($_SESSION["foodJSON"], true);
     $output["couponId"] = json_decode($_SESSION["couponJSON"], true);
     $output["user"] = $_SESSION["user"];
-    if(isset($_SESSION["products_order"])) {
-        $output["product"] = $_SESSION["products_order"];
-    }
-    if(isset($_SESSION["food_order"])) {
-        $output["food"] = json_decode($_SESSION["food_order"],true);
-    }
     echo json_encode($output, JSON_UNESCAPED_UNICODE);
 ?>
