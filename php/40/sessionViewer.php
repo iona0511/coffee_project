76ad1c8ed8547f00616958090a881c896c135eff
hunterluo <hunter850711@gmail.com>
2022-06-09 22:51:40 +0800

    session viewer新增displayTotal

--- php/40/sessionViewer.php
+++ php/40/sessionViewer.php
@@ -5,6 +5,7 @@
     $output["product"] = json_decode($_SESSION["productJSON"], true);
     $output["food"] = json_decode($_SESSION["foodJSON"], true);
     $output["couponId"] = json_decode($_SESSION["couponJSON"], true);
+    $output["displayTotal"] = json_decode($_SESSION["displayTotal"], true);
     $output["user"] = $_SESSION["user"];
     echo json_encode($output, JSON_UNESCAPED_UNICODE);
 ?>
