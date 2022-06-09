4ce51687bc0a5354caa017e775773f4a16f968e2
hunterluo <hunter850711@gmail.com>
2022-06-09 22:52:16 +0800

    write session 新增displayTotal

--- php/40/cart_write_session_api.php
+++ php/40/cart_write_session_api.php
@@ -5,5 +5,6 @@
     $_SESSION["productJSON"] =  $_POST["product"];
     $_SESSION["foodJSON"] = $_POST["food"];
     $_SESSION["couponJSON"] = $_POST["selectedCoupon"];
+    $_SESSION["displayTotal"] = $_POST["displayTotal"];
     echo json_encode(true, JSON_UNESCAPED_UNICODE);
 ?>
