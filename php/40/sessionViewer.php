7e100a7b93f9a4cd54bc2de2696e926d6c6e03a3
hunterluo <hunter850711@gmail.com>
2022-06-10 01:49:08 +0800

    session viewer 新增判斷是否為空值

--- php/40/sessionViewer.php
+++ php/40/sessionViewer.php
@@ -2,10 +2,24 @@
     if(!session_id()) {
         session_start();
     }
-    $output["product"] = json_decode($_SESSION["productJSON"], true);
-    $output["food"] = json_decode($_SESSION["foodJSON"], true);
-    $output["couponId"] = json_decode($_SESSION["couponJSON"], true);
-    $output["displayTotal"] = json_decode($_SESSION["displayTotal"], true);
-    $output["user"] = $_SESSION["user"];
+    if(isset($_SESSION["productJSON"])) {
+        $output["product"] = json_decode($_SESSION["productJSON"], true);
+    }
+    if(isset($_SESSION["foodJSON"])) {
+        $output["food"] = json_decode($_SESSION["foodJSON"], true);
+    }
+    if(isset($_SESSION["couponJSON"])) {
+        $output["couponId"] = json_decode($_SESSION["couponJSON"], true);
+    }
+    if(isset($_SESSION["displayTotal"])) {
+        $output["displayTotal"] = json_decode($_SESSION["displayTotal"], true);
+    }
+    if(isset($_SESSION["newestOrder"])) {
+        $output["newestOrder"] = json_decode($_SESSION["newestOrder"], true);
+    }
+    if(isset($_SESSION["user"])) {
+        $output["user"] = $_SESSION["user"];
+    }
+    
     echo json_encode($output, JSON_UNESCAPED_UNICODE);
 ?>
