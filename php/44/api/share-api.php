c9cf72561d6aec9ef9b58d0d59ba7922f4491cb9
Tommy <sky003428@gmail.com>
2022-06-09 16:57:33 +0800

    share html

--- php/44/api/share-api.php
+++ php/44/api/share-api.php
@@ -1,7 +1,13 @@
 <?php
 require  dirname(dirname(__DIR__, 2)) . '/parts/connect_db.php';
 
-$sql = "SELECT * FROM `post_img`";
+
+// 排序為 熱門指數為comments*2+like,降冪排序,再排序文章創建時間,編輯時間
+$sql = "SELECT p.*,pi.img_name,pi.sort FROM `post` p 
+JOIN `post_img` pi ON p.sid = pi.post_sid
+WHERE pi.sort=1
+ORDER BY `likes` +`comments`*2 DESC,`created_at` DESC,`updated_at` DESC";
+
 
 
 $rows = $pdo->query($sql)->fetchAll();
