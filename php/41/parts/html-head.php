6486263bf8e0d111223a484d2d8b9da905428e82
Trista <hua2367@gmail.com>
2022-06-12 10:42:28 +0800

    優惠券前台

--- php/41/parts/html-head.php
+++ php/41/parts/html-head.php
@@ -7,6 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="../../css/reset.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+    <link rel="stylesheet" href="fontawesome/css/all.css" />
+    <link rel="stylesheet" href="." />
     <style>
         *{
             box-sizing: border-box;
